6854e7daeec6552d9fd5ec2f572ba50a35d00543
viktoralmqvist <viktor.a.almqvist@gmail.com>
2022-02-11 14:02:28 +0100

    Inform others with this addon when starting combat logging

--- GurkLog.lua
+++ GurkLog.lua
@@ -1,6 +1,9 @@
 ﻿local frame = CreateFrame("FRAME");
 frame:RegisterEvent("RAID_INSTANCE_WELCOME");
 frame:RegisterEvent("ADDON_LOADED");
+frame:RegisterEvent("CHAT_MSG_ADDON");
+
+local prefix = "gurklogprefix";
 
 local green = "|cff00ff00";
 local red = "|cffff0000";
@@ -8,82 +11,90 @@ local nocolor = "|r";
 local on = green .. "ON" .. nocolor;
 local off = red .. "OFF" .. nocolor;
 
-function frame:OnEvent(event, arg1, ...)
-  debugLog(event);
+local playerName = UnitName("player");
 
-  if event == "RAID_INSTANCE_WELCOME" then
-    if isRaidInstance() then
-      GurkStartLogging();
-    end
+function frame:OnEvent(event, arg1, arg2, ...)
+   debugLog(event);
 
-  elseif event == "ADDON_LOADED" and arg1 == "GurkLog" then
-    init();
+   if event == "RAID_INSTANCE_WELCOME" then
+      if isRaidInstance() then
+	 GurkStartLogging();
+      end
+      
+   elseif event == "ADDON_LOADED" and arg1 == "GurkLog" then
+      init();
 
-  end
+   elseif event == "CHAT_MSG_ADDON" and arg1 == prefix then
+      print(arg2);
+
+   end
 end
 
 frame:SetScript("OnEvent", frame.OnEvent);
 
 SLASH_HAVEWEMET1 = "/gurk";
 function SlashCmdList.HAVEWEMET(msg)
- if msg == "start" then
-   GurkStartLogging();
+   if msg == "start" then
+      GurkStartLogging();
 
- elseif msg == "stop" then
-   GurkStopLogging();
+   elseif msg == "stop" then
+      GurkStopLogging();
 
- elseif msg == "debug" then
-   GurkDebug = not GurkDebug;
+   elseif msg == "debug" then
+      GurkDebug = not GurkDebug;
 
- else
-   GurkCheckLogging();
+   else
+      GurkCheckLogging();
 
- end
+   end
 end
 
 function init()
-  if GurkDebug == nil then
-    GurkDebug = false;
-  end
+   C_ChatInfo.RegisterAddonMessagePrefix(prefix);
 
-  if isRaidInstance() then
-    GurkStartLogging();
-  end
+   if GurkDebug == nil then
+      GurkDebug = false;
+   end
+
+   if isRaidInstance() then
+      GurkStartLogging();
+   end
 end
 
 function isRaidInstance()
-  local _, zoneType = GetInstanceInfo();
-  return zoneType == "raid";
+   local _, zoneType = GetInstanceInfo();
+   return zoneType == "raid";
 end
 
 function debugLog(msg)
-  if GurkDebug then
-    print("Gurk debug: " .. msg);
-  end
+   if GurkDebug then
+      print("Gurk debug: " .. msg);
+   end
 end
 
 function GurkCheckLogging()
-  if LoggingCombat() then
-   print("Combat logging is " .. on);
-  else
-   print("Combat logging is " .. off);
-  end
+   if LoggingCombat() then
+      print("Combat logging is " .. on);
+   else
+      print("Combat logging is " .. off);
+   end
 end
 
 function GurkStartLogging()
-  if LoggingCombat() then
-    print("Combat logging is already " .. on);
-  else
-    LoggingCombat(true);
-    print("Combat logging turned " .. on);
-  end
+   if LoggingCombat() then
+      print("Combat logging is already " .. on);
+   else
+      LoggingCombat(true);
+      C_ChatInfo.SendAddonMessage(prefix, playerName .. " started combat logging", "RAID");
+      print("Combat logging turned " .. on);
+   end
 end
 
 function GurkStopLogging()
-  if LoggingCombat() then
-    LoggingCombat(false);
-    print("Combat logging turned " .. off);
-  else
-    print("Combat logging is already " .. off);
-  end
+   if LoggingCombat() then
+      LoggingCombat(false);
+      print("Combat logging turned " .. off);
+   else
+      print("Combat logging is already " .. off);
+   end
 end
